505d1eafa1bab686e845a6a5076a2792fd32e1cf
Vishnu Narang <vishnunarang2000@gmail.com>
2023-09-20 23:56:25 -0700

    Trying a different way to render the react app

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-ReactDOM.render(<App />, document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
