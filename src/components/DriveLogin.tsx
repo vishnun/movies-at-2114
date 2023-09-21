82969103c952ac3ad22e00947dc6af1dae891b52
Vishnu Narang <vishnunarang2000@gmail.com>
2023-09-21 01:29:41 -0700

    Making a temp change to make google login work in github pages

--- src/components/DriveLogin.tsx
+++ src/components/DriveLogin.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { GoogleLogin } from 'react-google-login';
-
-
+// import { GoogleLogin } from 'react-google-login';
+//
+//
 interface DriveLoginProps {
   onSuccess: (token: string) => void;
 }
-
+//
 const DriveLogin: React.FC<DriveLoginProps> = ({ onSuccess }) => {
-  const responseGoogle = (response: any) => {
-      debugger
-    if (response.accessToken) {
-      onSuccess(response.accessToken);
-    } else {
-      console.error('Google Sign-In failed.');
-    }
-  };
-
-  return (
-    <div>
-      <h2>Google Drive Integration</h2>
-      <GoogleLogin
-        clientId="742371489419-9ueaesa8q5q7gnjumikkds0ah4o1fuu5.apps.googleusercontent.com"
-        buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-    </div>
-  );
+//   const responseGoogle = (response: any) => {
+//       debugger
+//     if (response.accessToken) {
+//       onSuccess(response.accessToken);
+//     } else {
+//       console.error('Google Sign-In failed.');
+//     }
+//   };
+//
+//   return (
+//     <div>
+//       <h2>Google Drive Integration</h2>
+//       <GoogleLogin
+//         clientId="742371489419-9ueaesa8q5q7gnjumikkds0ah4o1fuu5.apps.googleusercontent.com"
+//         buttonText="Login with Google"
+//         onSuccess={responseGoogle}
+//         onFailure={responseGoogle}
+//         cookiePolicy={'single_host_origin'}
+//       />
+//     </div>
+//   );
+    return (<div>TEST</div>);
 };
-
+//
 export default DriveLogin;
