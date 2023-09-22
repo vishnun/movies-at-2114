f4e47bf85f1e6aad4a8b59fa265106d30309e12e
Vishnu Narang <vishnunarang2000@gmail.com>
2023-09-21 23:04:48 -0700

    Integrating material react ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import {googleLogout, useGoogleLogin} from '@react-oauth/google';
+import axios from 'axios';
+import logo from './2114-logo.png'
+import theme from './theme';
+import {
+    AppBar,
+    Box,
+    Button,
+    Container,
+    Avatar,
+    IconButton,
+    Menu,
+    MenuItem,
+    Tooltip,
+    ThemeProvider,
+    Toolbar,
+    Typography,
+    ButtonProps, styled, SvgIcon, Icon,
+} from "@mui/material";
+import {grey} from '@mui/material/colors';
+import MenuIcon from '@mui/icons-material/Menu';
+import AdbIcon from '@mui/icons-material/Adb';
+import GoogleIcon from '@mui/icons-material/Google';
+
+const pages = ['Movies', 'Watch Now'];
+const settings = ['Profile', 'Dashboard', 'Logout']
+
 // import React, { useState, useEffect } from 'react';
 // import { GoogleAuth } from 'google-identity-services';
 // import { HashRouter as Router, Route, Routes } from 'react-router-dom';
@@ -38,9 +66,6 @@
 //
 // export default App;
 
-import React, { useState, useEffect } from 'react';
-import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
 
 // Define a type for the user profile
 type UserProfile = {
@@ -49,6 +74,13 @@ type UserProfile = {
     email: string;
     // Add other profile properties if needed
 };
+const LoginButton = styled(Button)<ButtonProps>(({theme}) => ({
+    color: theme.palette.getContrastText(grey[50]),
+    backgroundColor: grey[50],
+    '&:hover': {
+        backgroundColor: grey[50],
+    },
+}));
 
 function App() {
     const [user, setUser] = useState<any>(null); // Use 'any' temporarily
@@ -81,25 +113,162 @@ function App() {
         setProfile(null);
     };
 
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = (setting: any) => {
+        if (setting === 'Logout') {
+            logOut();
+        }
+        setAnchorElUser(null);
+    };
+
     return (
-        <div>
-            <h2>React Google Login</h2>
-            <br />
-            <br />
-            {profile ? (
-                <div>
-                    <img src={profile.picture} alt="user image" />
-                    <h3>User Logged in</h3>
-                    <p>Name: {profile.name}</p>
-                    <p>Email Address: {profile.email}</p>
-                    <br />
-                    <br />
-                    <button onClick={logOut}>Log out</button>
-                </div>
-            ) : (
-                <button onClick={() => login()}>Sign in with Google 🚀</button>
-            )}
-        </div>
+        <ThemeProvider theme={theme}>
+            <div>
+                <AppBar position="static">
+                    <Container maxWidth="xl">
+                        <Toolbar disableGutters>
+                            <Avatar alt="Movies 2114" src={logo}/>
+
+                            <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenNavMenu}
+                                    color="inherit"
+                                >
+                                    <MenuIcon/>
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorElNav}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                    open={Boolean(anchorElNav)}
+                                    onClose={handleCloseNavMenu}
+                                    sx={{
+                                        display: {xs: 'block', md: 'none'},
+                                    }}
+                                >
+                                    {pages.map((page) => (
+                                        <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                            <Typography textAlign="center">{page}</Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                            <AdbIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1}}/>
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                component="a"
+                                href="/"
+                                sx={{
+                                    mr: 2,
+                                    display: {xs: 'flex', md: 'none'},
+                                    flexGrow: 1,
+                                    fontFamily: 'monospace',
+                                    fontWeight: 700,
+                                    letterSpacing: '.3rem',
+                                    color: 'inherit',
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                LOGO
+                            </Typography>
+                            <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                                {pages.map((page) => (
+                                    <Button
+                                        key={page}
+                                        onClick={handleCloseNavMenu}
+                                        sx={{my: 2, color: 'white', display: 'block'}}
+                                    >
+                                        {page}
+                                    </Button>
+                                ))}
+                            </Box>
+
+                            {profile && (
+                                <Box sx={{flexGrow: 0}}>
+
+                                    <Tooltip title="Open settings">
+                                        <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                            <Avatar alt="Remy Sharp" src={profile.picture}/>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Menu
+                                        sx={{mt: '45px'}}
+                                        id="menu-appbar"
+                                        anchorEl={anchorElUser}
+                                        anchorOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        open={Boolean(anchorElUser)}
+                                        onClose={handleCloseUserMenu}
+                                    >
+                                        {settings.map((setting) => (
+                                            <MenuItem key={setting} onClick={() => handleCloseUserMenu(setting)}>
+                                                <Typography textAlign="center">{setting}</Typography>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+                                </Box>
+                            )}
+                            {!profile && (
+                                <LoginButton startIcon={<GoogleIcon/>} variant="contained" color="primary"
+                                        onClick={() => login()}>Sign in</LoginButton>
+                            )}
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </div>
+            <div>
+                <h2>React Google Login</h2>
+                <br/>
+                <br/>
+                {profile ? (
+                    <div>
+                        <img src={profile.picture} alt="user image"/>
+                        <h3>User Logged in</h3>
+                        <p>Name: {profile.name}</p>
+                        <p>Email Address: {profile.email}</p>
+                        <br/>
+                        <br/>
+                        <Button onClick={logOut}>Log out</Button>
+                    </div>
+                ) : (
+                    <Button startIcon={<GoogleIcon/>} variant="contained" color="primary" onClick={() => login()}>Sign
+                        in with Google</Button>
+                )}
+            </div>
+        </ThemeProvider>
     );
 }
 
---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+// theme.ts
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976D2',
+        },
+        secondary: {
+            main: '#E91E63',
+        },
+    },
+});
+
+export default theme;
